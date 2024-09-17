598b0b4f33b667fafe64d2b73e153ca2f9f65d7d
wjohns250 <wjohns27@uwyo.edu>
2024-09-17 21:46:22 +0000

    Completed Lab02

---
+++ Lab01.py
@@ -0,0 +1,26 @@
+# William Johnson
+# UWyo COSC 1010
+# 9/10/2024
+# Lab 01
+# Lab Section 10
+# No Sources
+
+print("hello world")
+
+x = 5
+y = 6.5
+z = x + y
+
+print(z)
+
+first = "William"
+last = "Johnson"
+major = "Mechanical Engineering"
+
+print("My name is: ", first, " ", last)
+print("Major: ", major)
+
+x, y = 8, 4
+
+print(x/y)
+
